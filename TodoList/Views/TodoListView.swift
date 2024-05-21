7fb3e9362fcb31d58c441868eaeed978c35e0b79
Matthew Zhang <gzhang@lcs.on.ca>
2024-05-21 09:26:29 -0400

    Followed the instruction on the part 1

--- TodoList/Views/TodoListView.swift
+++ TodoList/Views/TodoListView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct TodoListView: View {
+    
+    // Stored properties
+    
+    // The item currently being added
+    @State var newItemDescription: String = ""
+    
+    // The search text
+    @State var searchText = ""
+    
+    // Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            
+            VStack {
+                
+                List {
+                    
+                }
+                .searchable(text:$searchText)
+                
+                HStack {
+                }
+                .padding(20)
+                
+            }
+            .navigationTitle("To do")
+            
+        }
     }
 }
 
