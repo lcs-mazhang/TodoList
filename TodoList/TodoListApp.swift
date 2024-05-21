949d76c5def2e4fef31774582a5ec58f2e921707
Matthew Zhang <gzhang@lcs.on.ca>
2024-05-21 08:58:58 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by GengYu Zhang on 2024-05-21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
