14b094e6c3506a4497ca44d19b8a17c4b1459f9e
Matthew Zhang <gzhang@lcs.on.ca>
2024-05-21 09:52:04 -0400

    Got a basic static interface created

--- TodoList/Views/TodoListView.swift
+++ TodoList/Views/TodoListView.swift
@@ -26,10 +26,30 @@ struct TodoListView: View {
                 
                 List {
                     
-                    Text("Study for Chemistry quiz")
-                    Text("Finish Computer Science assignent")
-                    Text("Go for a run around campus")
+                    Label(
+                    title: {
+                        Text("study for Chemistry quiz")
+                    }, icon: {
+                        Image(systemName: "circle")
+                    }
+                )
                     
+                    Label(
+                     title: {
+                         Text("Finish Computer Science assignment")
+                     }, icon: {
+                         Image(systemName: "circle")
+                     }
+                 )
+                    
+                    Label(
+                     title: {
+                         Text("Go for a run around campus")
+                     }, icon: {
+                         Image(systemName: "circle")
+                     }
+                 )
+                
                 }
                 .searchable(text:$searchText)
                 
